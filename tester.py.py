208dda30af400115acdc7ac17b31c2b5dc4705da
Chaehyun,lee <chyomi0215@naver.com>
2024-11-15 21:47:57 +0900

    tryy

---
+++ tester.py.py
@@ -0,0 +1,13 @@
+import geo.utils as utils
+
+#calculate the length of c
+
+a, b = 3,4
+c = utils.pythagoras(a,b)
+print("c = ", c)
+
+#calculate the area of circle
+
+r = 10
+area = utils.circle(r)
+print("area = ", area)
